ce143726d43c8de6adfe036da3336500e0f4bfd8
Connor Hilarides <connorcpu@live.com>
2016-06-18 14:28:59 -0700

    Working on sprite loading

--- Source/AssetPipeline/SpriteLoader.cpp
+++ Source/AssetPipeline/SpriteLoader.cpp
@@ -1,6 +1,7 @@
 #include "SpriteLoader.h"
 #include "SpritePack.h"
 #include "MMap.h"
+#include  "SpriteDesc.h"
 
 #include <connorlib/serialization/toml.h>
 #include <connorlib/imageload.h>
@@ -20,7 +21,8 @@ SpritePack *SpriteLoader::Load(const std::string &pack)
     if (loaded_it != loaded_packs_.end())
         return loaded_it->second.get();
 
-    fs::path desc_path = fs::current_path() / "Assets" / "Sprites" / pack / "Pack.toml";
+	fs::path pack_folder = fs::current_path() / "Assets" / "Sprites" / pack;
+    fs::path desc_path = pack_folder / "Pack.toml";
 
     MMap mmap;
     if (!mmap.Open(desc_path))
@@ -47,9 +49,36 @@ SpritePack *SpriteLoader::Load(const std::string &pack)
     // Read in the sprite list / animated gif
     if (pack_type == SpritePackType::Sprites)
     {
-        for (auto &sprite_desc : config_root["Sprites"]->GetArray())
+		std::vector<ImageLoad::Image> images;
+		bool has_size = false, consistent_size = true;
+		uint32_t pack_width, pack_height;
+
+        for (auto &sprite_data : config_root["Sprite"]->GetArray())
         {
-            // todo
+			SpriteDesc sprite_desc;
+			sprite_desc.Deserialize(sprite_data.GetTable(), pack_folder);
+
+			auto img_id = sprite_desc.GetImageId();
+			auto image = sprite_desc.GetImage(img_id);
+
+			// Consistent size is a lot easier, but sometimes packs need to contain "assorted" sprites.
+			// I'll work out how to support that :P
+			uint32_t width, height;
+			image.GetSize(&width, &height);
+			if (has_size)
+			{
+				consistent_size = consistent_size && width == pack_width && height == pack_height;
+				pack_width = (std::max)(width, pack_width);
+				pack_height = (std::max)(height, pack_height);
+			}
+			else
+			{
+				pack_width = width;
+				pack_height = height;
+				has_size = true;
+			}
+
+			images.emplace_back(std::move(image));
         }
     }
 
